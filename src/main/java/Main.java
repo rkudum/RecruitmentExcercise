e6f9233bd3a3f4dd6c9f5df985f96ba0f9010980
Pankaj Pardasani <pankaj.d.p@me.com>
2017-12-26 10:05:56 +0000

    Adding comments for improvements in DTO; Adding error repo for unparsed entries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         UfoSightingService service = new UfoSightingServiceImpl();
 
-        service.getAllSightings().stream().forEach(ufoSight -> System.out.println(ufoSight));
+        System.out.println("Total sightings are " +service.getAllSightings().stream().count());
 
         System.out.println(String.format("Total number of UFO incidents in Oct 1995 were %d", service.search(1995, 10).size()));
 
--- src/main/java/ufo/service/impl/UfoSightingServiceImpl.java
+++ src/main/java/ufo/service/impl/UfoSightingServiceImpl.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
@@ -18,17 +16,20 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class UfoSightingServiceImpl implements UfoSightingService {
-
     private static final Logger LOG = Logger.getLogger(UfoSightingServiceImpl.class.getName());
+    private static final Function<String, String> NULLSAFEGET = s -> s == null ? "" : s;
 
     private final Path ufoFilePath;
+    private Queue<String> sightingErrorRepository;
 
     public UfoSightingServiceImpl() {
-        ufoFilePath = Paths.get(UfoSightingServiceImpl.class.getResource("/ufo_awesome.tsv").getPath());
+        this.ufoFilePath = Paths.get(UfoSightingServiceImpl.class.getResource("/ufo_awesome.tsv").getPath());
+        this.sightingErrorRepository = new LinkedList<>();
     }
 
     public UfoSightingServiceImpl(String filePathURI) {
         this.ufoFilePath = Paths.get(filePathURI);
+        this.sightingErrorRepository = new LinkedList<>();
     }
 
     @Override
@@ -58,17 +59,18 @@ public class UfoSightingServiceImpl implements UfoSightingService {
                     .map(line -> {
                         String[] lineToken = line.split("\t");
 
-                        if (lineToken.length == 6) {
-                            return parseTokens(lineToken);
+                        if (lineToken.length != 6) {
+                            sightingErrorRepository.offer(Stream.of(lineToken).collect(Collectors.joining(",")));
+                            //LOG.warning(() -> String.format("The UFO entry in file has %d entries (hence skipped)", lineToken.length));
+                            return null;
                         }
 
-                        LOG.warning(() -> String.format("The UFO entry in file is not correctly tokenized and hence skipping the record!"));
-
-                        return null;
+                        return parseTokens(lineToken);
                     })
                     .collect(Collectors.toList());
         } catch (IOException ioe) {
-            System.out.println(ioe);
+            LOG.severe("Error whilst reading feed file");
+            throw new RuntimeException("Error whilst reading feed file", ioe);
         }
 
         return ufoSightingList == null ? Collections.emptyList() : ufoSightingList;
@@ -84,7 +86,4 @@ public class UfoSightingServiceImpl implements UfoSightingService {
     private UfoSighting parseTokens(String[] lineToken) {
         return new UfoSighting(lineToken[0].trim(), lineToken[1].trim(), lineToken[2].trim(), lineToken[3].trim(), lineToken[4].trim(), lineToken[5].trim());
     }
-
-    private Function<String, String> NULLSAFEGET = s -> s == null ? "" : s;
-
 }
