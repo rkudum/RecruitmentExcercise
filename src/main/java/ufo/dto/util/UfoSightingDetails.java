8c32efed460e5f63cfb70aa83089ddf427c32b8c
Seyalvp <seyal.v.p@gmail.com>
2018-01-09 19:16:19 +0000

    Unit tests added for UfoSightingDetails

--- src/main/java/ufo/dto/util/UfoSightingDetails.java
+++ src/main/java/ufo/dto/util/UfoSightingDetails.java
@@ -5,11 +5,16 @@ import ufo.dto.UfoSighting;
 public class UfoSightingDetails {
 
     public static String getDateSeenYYYYMM(UfoSighting ufoSighting){
-        String fulldateSeen = ufoSighting.getDateSeen();
-        if(fulldateSeen.length()>=6) {
-            return fulldateSeen.substring(0, 6);
-        }else{
-            return fulldateSeen;
+        String yearAndMonth = null;
+
+        if(null != ufoSighting) {
+            String fulldateSeen = ufoSighting.getDateSeen();
+            if (fulldateSeen.length() >= 6) {
+                yearAndMonth =  fulldateSeen.substring(0, 6);
+            } else {
+                yearAndMonth = fulldateSeen;
+            }
         }
+        return yearAndMonth;
     }
 }
